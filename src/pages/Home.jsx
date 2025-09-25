a3879aa023c5686d5e3ffa0b0db75fa1ce9093c2
ronquecj <ronquechristian@gmail.com>
2025-09-25 11:46:19 +0800

    test - p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,17 @@ export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Similique, alias laboriosam magnam error magni recusandae
+        distinctio illum officia nesciunt facere optio aliquam quae
+        voluptas minima. Quisquam, suscipit. Placeat laboriosam quam
+        suscipit recusandae labore sapiente et optio maiores, soluta
+        sequi est nam excepturi sunt delectus saepe? Officiis
+        voluptatibus accusamus modi tempore commodi ut expedita
+        repudiandae, quod minima, perferendis tenetur fuga? Quis
+        mollitia doloribus tenetur pariatur recusandae?
+      </p>
     </div>
   );
 };
