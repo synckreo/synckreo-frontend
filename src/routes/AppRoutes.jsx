81687602b56f83b30c137474544cb59b8215ad44
‎ <benidi.janeo@gmail.com>
2025-09-28 09:55:53 +0800

    Added route for Explore page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '../components/layouts/MainLayout';
 import { Home } from '../pages/home/Home';
+import { Explore } from '../pages/explore/Explore';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
       </Route>
     </Routes>
   );
