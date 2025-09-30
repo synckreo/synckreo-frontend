fbf16e2b4b3ae3e6cfb1f53b85099fd022282df8
Jay-B <144989942+JayBDC@users.noreply.github.com>
2025-09-30 17:33:53 +0800

    Footer minor update

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -72,22 +72,51 @@ export const Footer = () => {
             FAQs
           </a>
         </div>
-        <div className="align-items-center flex flex-col gap-2 mt-10">
+        <div className="flex flex-col gap-2 mt-10">
           <h3 className="text-center text-lg font-semibold mb-2">
             Download
           </h3>
-          <div className="mb-4 align-items-center flex gap-4 flex-row">
-            <button className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors mb-2">
-              <i className="fab fa-apple mr-2 text-sm">
-                Download on the
-              </i>
-              App Store
+          <div className="flex gap-5 flex-row">
+            <button className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors mb-2 flex-row flex items-center">
+              <svg
+                className="w-[40px] h-[40px] text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M17.537 12.625a4.421 4.421 0 0 0 2.684 4.047 10.96 10.96 0 0 1-1.384 2.845c-.834 1.218-1.7 2.432-3.062 2.457-1.34.025-1.77-.794-3.3-.794-1.531 0-2.01.769-3.275.82-1.316.049-2.317-1.318-3.158-2.532-1.72-2.484-3.032-7.017-1.27-10.077A4.9 4.9 0 0 1 8.91 6.884c1.292-.025 2.51.869 3.3.869.789 0 2.27-1.075 3.828-.917a4.67 4.67 0 0 1 3.66 1.984 4.524 4.524 0 0 0-2.16 3.805m-2.52-7.432A4.4 4.4 0 0 0 16.06 2a4.482 4.482 0 0 0-2.945 1.516 4.185 4.185 0 0 0-1.061 3.093 3.708 3.708 0 0 0 2.967-1.416Z" />
+              </svg>
+
+              <div className="mx-2 text-left">
+                <i className="mr-2 text-sm">Download on the</i>
+                <br />
+                App Store
+              </div>
             </button>
-            <button className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors">
-              <i className="fab fa-google-play mr-2 text-sm">
-                Get it on
-              </i>
-              Google Play
+            <button className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors mb-2 flex-row flex items-center">
+              <svg
+                class="w-[40px] h-[40px] text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 4a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V4Zm12 12V5H7v11h10Zm-5 1a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <div className="mx-2 text-left">
+                <i className="text-sm">Get it on</i>
+                <br />
+                Google Play
+              </div>
             </button>
           </div>
         </div>
