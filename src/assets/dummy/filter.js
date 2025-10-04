9370bc89e3699178a81000ea272c00f36bb34fe8
‎ <benidi.janeo@gmail.com>
2025-10-04 15:43:21 +0800

    filter dummy data

---
+++ src/assets/dummy/filter.js
@@ -0,0 +1,10 @@
+export const filters = {
+  tags: [
+    'House and Building Design',
+    '3D Building Models',
+    'Green and Eco-Friendly Design',
+    'Drawing Floor Plans',
+    'Planning Room Layouts',
+    'Managing Projects',
+  ],
+};
