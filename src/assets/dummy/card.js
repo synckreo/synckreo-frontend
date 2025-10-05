5e28600c54f1dc07989117c53f39b3d1db99559f
‎ <benidi.janeo@gmail.com>
2025-10-06 06:52:31 +0800

    added 4 new professional dummy data

--- src/assets/dummy/card.js
+++ src/assets/dummy/card.js
@@ -2,6 +2,10 @@ import card_professional_01 from '../images/card-images/card_professional_01.web
 import card_professional_02 from '../images/card-images/card_professional_02.webp';
 import card_professional_03 from '../images/card-images/card_professional_03.webp';
 import card_professional_04 from '../images/card-images/card_professional_04.webp';
+import card_professional_05 from '../images/card-images/card_professional_05.webp';
+import card_professional_06 from '../images/card-images/card_professional_06.webp';
+import card_professional_07 from '../images/card-images/card_professional_07.webp';
+import card_professional_08 from '../images/card-images/card_professional_08.webp';
 import card_company_01 from '../images/card-images/card_company_01.webp';
 import card_company_02 from '../images/card-images/card_company_02.webp';
 import card_company_03 from '../images/card-images/card_company_03.webp';
@@ -46,6 +50,46 @@ export const data = {
       description:
         'Skilled in installing energy-efficient aircon and heating units for homes and offices.',
       image: card_professional_04,
+      rating: 4.6,
+      reviewCount: 48,
+    },
+    {
+      id: 5,
+      name: 'Ava Sullivan',
+      category: 'Architect',
+      description:
+        'Designs beautiful, functional spaces with a focus on sustainability and client vision.',
+      image: card_professional_05,
+      rating: 4.6,
+      reviewCount: 48,
+    },
+    {
+      id: 6,
+      name: 'Chloe Nguyen',
+      category: 'Masonry Specialist',
+      description:
+        'Skilled in brick, stone, and concrete work—known for solid craftsmanship and beautiful finishes.',
+      image: card_professional_06,
+      rating: 4.6,
+      reviewCount: 48,
+    },
+    {
+      id: 7,
+      name: 'Noah Carter',
+      category: 'General Contructor',
+      description:
+        'Oversees all phases of residential and commercial builds, ensuring projects are completed on time and within budget.',
+      image: card_professional_07,
+      rating: 4.6,
+      reviewCount: 48,
+    },
+    {
+      id: 8,
+      name: 'Liam Brooks',
+      category: 'Structural Engineer',
+      description:
+        'Ensures buildings are safe, stable, and built to code with expert analysis and planning.',
+      image: card_professional_08,
       rating: 4.6,
       reviewCount: 48,
     },
