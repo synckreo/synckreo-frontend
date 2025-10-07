7237bc5cd174ea558c322798d4da70a245c932a6
‎ <benidi.janeo@gmail.com>
2025-10-07 17:05:05 +0800

    init

---
+++ src/components/profile/shared/ProfileHeader.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { Button } from '../../../components/ui/buttons/Button';
+
+import locationIcon from '../../../assets/icons/location.svg';
+import starIcon from '../../../assets/icons/star.svg';
+
+export const ProfileHeader = ({
+  location,
+  role,
+  name,
+  reviews,
+  rating,
+  likes,
+  followers,
+  following,
+  badge,
+  avatar,
+}) => {
+  const { city, state } = location;
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <div className="mb-4">
+        <img
+          className="h-[150px] w-[150px] rounded-[100px]"
+          src={avatar}
+          alt="Avatar"
+        />
+      </div>
+      <div className="flex items-center gap-2">
+        <h3 className="text-5xl font-semibold">{name}</h3>
+        <span className="text-primary rounded-[6px] bg-[#F57C001A] px-3 py-2 text-xs font-medium">
+          {badge}
+        </span>
+      </div>
+      <div className="flex gap-2">
+        <div className="flex items-center gap-2">
+          <span>
+            <img src={locationIcon} alt="" />
+          </span>
+          <span>
+            {city}, {state}
+          </span>
+        </div>
+        <div className="flex gap-2">
+          <div className="flex items-center gap-2">
+            <span>
+              <img src={starIcon} alt="" />
+            </span>
+            <span>{rating}</span>
+          </div>
+          <span>({reviews})</span>
+        </div>
+      </div>
+      {/* Stats */}
+      <div className="flex gap-4">
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-medium">{followers}</span>
+          <span className="text-sm text-[#262626B2]">Followers</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-medium">{following}</span>
+          <span className="text-sm text-[#262626B2]">Following</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-medium">{likes}</span>
+          <span className="text-sm text-[#262626B2]"> Likes</span>
+        </div>
+      </div>
+
+      <div className="mt-6">
+        <Button title="Edit Profile" variant="outline" />
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/profile/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ProfessionalProfile } from '../../components/profile/Professional/ProfessionalProfile';
+import { ProjectGallery } from '../../components/profile/Professional/ProjectGallery';
+
+export const ProfilePage = () => {
+  return <ProfessionalProfile />;
+};
