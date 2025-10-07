f124a7355dcd783d563825470a20e217996857ac
‎ <benidi.janeo@gmail.com>
2025-10-07 15:03:07 +0800

    Simplified truncate function to use tw default

--- src/components/ui/card/ProjectCard.jsx
+++ src/components/ui/card/ProjectCard.jsx
@@ -10,15 +10,13 @@ export const ProjectCard = ({
 }) => {
   return (
     <div
-      className="relative flex h-[230px] w-[360px] shrink-0 flex-col justify-end rounded-2xl bg-cover bg-center bg-no-repeat text-white"
+      className="relative flex h-[180px] w-[320px] shrink-0 flex-col justify-end rounded-2xl bg-cover bg-center bg-no-repeat text-white md:h-[230px] md:w-[360px]"
       style={{ backgroundImage: `url(${projectBackgroundImg})` }}
     >
       <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 rounded-b-2xl bg-gradient-to-t from-black/70 to-transparent" />
       <div className="relative z-10 flex justify-between gap-2 p-2">
-        <span className="text-lg font-semibold">
-          {projectName.length > 20
-            ? `${projectName.slice(0, 20)}...`
-            : projectName}
+        <span className="truncate text-base font-semibold md:text-lg">
+          {projectName}
         </span>
 
         <div className="flex gap-2">
