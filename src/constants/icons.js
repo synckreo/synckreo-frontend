5ac643d5a1b2149a6ee837d7de08bcb6131a7811
‎ <benidi.janeo@gmail.com>
2025-11-05 19:45:46 +0800

    constants for icons

---
+++ src/constants/icons.js
@@ -0,0 +1,39 @@
+import ArrowDown from '../assets/icons/arrow-down.svg';
+import Arrow from '../assets/icons/arrow.svg';
+import Calendar from '../assets/icons/calendar.svg';
+import Clock from '../assets/icons/clock.svg';
+import Dollar from '../assets/icons/dollar.svg';
+import Eco from '../assets/icons/eco.svg';
+import Eye from '../assets/icons/eye.svg';
+import FbLogo from '../assets/icons/fb-logo.svg';
+import Finance from '../assets/icons/finance.svg';
+import GmailLogo from '../assets/icons/gmail-logo.svg';
+import Heart from '../assets/icons/heart.svg';
+import Home from '../assets/icons/home.svg';
+import LinkedinLogo from '../assets/icons/linkedin-logo.svg';
+import Location from '../assets/icons/location.svg';
+import Saved from '../assets/icons/saved.svg';
+import SearchIcon from '../assets/icons/search-icon.svg';
+import Star from '../assets/icons/star.svg';
+import Tools from '../assets/icons/tools.svg';
+
+export const ICONS = {
+  arrowDown: ArrowDown,
+  arrow: Arrow,
+  calendar: Calendar,
+  clock: Clock,
+  dollar: Dollar,
+  eco: Eco,
+  eye: Eye,
+  fbLogo: FbLogo,
+  finance: Finance,
+  gmailLogo: GmailLogo,
+  heart: Heart,
+  home: Home,
+  linkedinLogo: LinkedinLogo,
+  location: Location,
+  saved: Saved,
+  search: SearchIcon,
+  star: Star,
+  tools: Tools,
+};
